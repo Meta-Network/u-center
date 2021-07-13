558f62f13f979940052debda90e3381da9c63d9d
xiaotiandada <istianlei@qq.com>
2021-07-14 01:55:57 +0800

    feat: scroll animated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,60 @@
+import React, { useCallback } from 'react'
 import Head from 'next/head'
-import { useSpringRef, useChain, useSpring, animated } from 'react-spring'
+import {
+  useSpringRef,
+  useChain,
+  useSpring,
+  animated,
+  Spring,
+} from 'react-spring'
 import styled from 'styled-components'
+import VisibilitySensor from 'react-visibility-sensor'
 import Item1 from '../assets/svg/item1.svg'
 import Item2 from '../assets/svg/item2.svg'
 import Item3 from '../assets/svg/item3.svg'
 import Item4 from '../assets/svg/item4.svg'
 
 export default function Home() {
+  // animated start
+  const animatedHead = useSpring({
+    from: { backgroundColor: '#fff' },
+    to: { backgroundColor: '#f5f5f5' },
+    duration: 1000,
+  })
 
-    // animated start
-    const animatedHead = useSpring({
-      from: { backgroundColor: '#fff' },
-      to: { backgroundColor: '#f5f5f5' },
-      duration: 1000,
-    })
+  const springRefTitle = useSpringRef()
+  const animatedTitle = useSpring({
+    from: { y: 10, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+    ref: springRefTitle,
+  })
 
-    const springRefTitle = useSpringRef()
-    const animatedTitle = useSpring({
-      from: { y: 10, opacity: 0 },
-      to: { y: 0, opacity: 1 },
-      ref: springRefTitle
-    })
+  const springRefDescription = useSpringRef()
+  const animatedDescription = useSpring({
+    from: { y: 10, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+    ref: springRefDescription,
+  })
 
-    const springRefDescription = useSpringRef()
-    const animatedDescription = useSpring({
-      from: { y: 10, opacity: 0 },
-      to: { y: 0, opacity: 1 },
-      ref: springRefDescription
-    })
+  useChain([springRefTitle, springRefDescription], [0, 0.3])
 
-    useChain([springRefTitle, springRefDescription], [0, 0.3])
+  const animatedItemLeft = useSpring({
+    from: { x: -40, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  })
+  const animatedItemRight = useSpring({
+    from: { x: 40, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  })
 
-    const animatedItemLeft = useSpring({
-      from: { x: -40, opacity: 0 },
-      to: { x: 0, opacity: 1 },
-    })
-    const animatedItemRight = useSpring({
-      from: { x: 40, opacity: 0 },
-      to: { x: 0, opacity: 1 },
-    })
-    // animated end
+  const animatedItemLeftConfig = useCallback((state) => {
+    return state ? { x: 0, opacity: 1 } : { x: -40, opacity: 0 }
+  }, [])
+  const animatedItemRightConfig = useCallback((state) => {
+    return state ? { x: 0, opacity: 1 } : { x: 40, opacity: 0 }
+  }, [])
+
+  // animated end
 
   return (
     <>
@@ -54,49 +69,125 @@ export default function Home() {
           Welcome to <a href="">UCenter!</a>
         </StyledHeadTitle>
 
-        <StyledHeadDescription style={{ ...animatedDescription }}>Get started</StyledHeadDescription>
+        <StyledHeadDescription style={{ ...animatedDescription }}>
+          Get started
+        </StyledHeadDescription>
       </StyledHead>
 
       <StyledItem>
-        <StyledItemImg style={{ ...animatedItemLeft }} src={Item1} alt="illustration" />
-        <StyledItemCard href="https://nextjs.org/docs" style={{ ...animatedItemRight }} >
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </StyledItemCard>
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemLeftConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemImg style={styles} src={Item1} alt="illustration" />
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemRightConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemCard href="https://nextjs.org/docs" style={styles}>
+                  <h2>Documentation &rarr;</h2>
+                  <p>
+                    Find in-depth information about Next.js features and API.
+                  </p>
+                </StyledItemCard>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
       </StyledItem>
 
       <StyledItem>
-        <StyledItemCard style={{ ...animatedItemLeft }} href="https://nextjs.org/learn" >
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </StyledItemCard>
-        <StyledItemImg src={Item2} style={{ ...animatedItemRight }} alt="illustration" />
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemLeftConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemCard href="https://nextjs.org/learn" style={styles}>
+                  <h2>Learn &rarr;</h2>
+                  <p>
+                    Learn about Next.js in an interactive course with quizzes!
+                  </p>
+                </StyledItemCard>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
+
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemRightConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemImg src={Item2} alt="illustration" style={styles} />
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
       </StyledItem>
 
       <StyledItem>
-        <StyledItemImg style={{ ...animatedItemLeft }} src={Item3} alt="illustration" />
-        <StyledItemCard
-          style={{ ...animatedItemRight }}
-          href="https://github.com/vercel/next.js/tree/master/examples"
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </StyledItemCard>
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemLeftConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemImg style={styles} src={Item3} alt="illustration" />
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
+
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemRightConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemCard
+                  style={styles}
+                  href="https://github.com/vercel/next.js/tree/master/examples"
+                >
+                  <h2>Examples &rarr;</h2>
+                  <p>
+                    Discover and deploy boilerplate example Next.js projects.
+                  </p>
+                </StyledItemCard>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
       </StyledItem>
 
       <StyledItem>
-        <StyledItemCard
-          style={{ ...animatedItemLeft }}
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </StyledItemCard>
-        <StyledItemImg style={{ ...animatedItemRight }} src={Item4} alt="illustration" />
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemLeftConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemCard
+                  style={styles}
+                  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                >
+                  <h2>Deploy &rarr;</h2>
+                  <p>
+                    Instantly deploy your Next.js site to a public URL with
+                    Vercel.
+                  </p>
+                </StyledItemCard>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
+
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring to={animatedItemRightConfig(isVisible)}>
+              {(styles) => (
+                <StyledItemImg style={styles} src={Item4} alt="illustration" />
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
       </StyledItem>
-      <StyledFooter>
-          © 2021 Meta Network All Rights Served
-      </StyledFooter>
+      <StyledFooter>© 2021 Meta Network All Rights Served</StyledFooter>
     </>
   )
 }
